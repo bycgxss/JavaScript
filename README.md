ec37b97c0d0e467abddc6eba365bccd2f4b156a9
bycgxss <bycgxss@gmail.com>
2021-11-22 10:14:02 +0800

    add deepCopy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 一些常用的 js 函数实现思路
 
 - [typeof](https://github.com/bycgxss/JavaScript/blob/master/typeof/index.js)
-- [深浅拷贝](https://github.com/bycgxss/JavaScript/blob/master/deepAndShallowCopy/shallowCopy.js)
+- [深浅拷贝](https://github.com/bycgxss/JavaScript/blob/master/deepAndShallowCopy)
---
+++ deepAndShallowCopy/deepCopy1.js
@@ -0,0 +1,25 @@
+/**
+ * JSON.parse(JSON.stringify()) 能满足基本的深拷贝
+ * bigInt 类型的值无法转换
+ * 对于属性值为 undefined symbol function 类型的，则会丢失对应的属性
+ * 对于属性值为 Error Regexp 这些类型的，属性值会变成空对象
+ * 对于属性值为 Date 类型的，属性值会被转换成字符串
+ */
+
+const obj = {
+  str: '1',
+  num: 1,
+  bool: true,
+  un: undefined,
+  null: null,
+  symbol: Symbol('xxx'),
+  // big: 10n,
+  children: { a: 'a' },
+  arr: [1, 2, 3],
+  fn: function () { },
+  date: new Date(),
+  err: new Error(),
+  reg: /123/
+}
+
+console.log(JSON.parse(JSON.stringify(obj)))

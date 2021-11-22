6d4b1bf800f1f512debb4faad085b19daeedfa44
bycgxss <bycgxss@gmail.com>
2021-11-22 09:54:29 +0800

    update shallowCopy

--- deepAndShallowCopy/shallowCopy.js
+++ deepAndShallowCopy/shallowCopy.js
@@ -4,29 +4,10 @@
  * 如果属性是引用数据类型，拷贝的是引用类型的内存地址
  */
 
-function shallowCopy (obj) {
+function shallowCopy (target) {
   if (typeof obj === 'object' && obj !== 'null') {
-    let copy = Array.isArray(obj) ? [] : {}
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        copy[key] = obj[key]
-      }
-    }
-    return copy
+    return Array.isArray(target) ? [...target] : { ...target }
   } else {
-    return obj
+    return target
   }
 }
-
-const arr = [1, 2, 3, [4, 5]]
-const arr1 = shallowCopy(arr)
-arr[0] = 0
-arr[3][0] = 5
-console.log(arr, arr1)
-
-
-const obj = { name: 'aaa', age: 24, hobby: ['Game', 'PingPong'] }
-const obj2 = shallowCopy(obj)
-obj.name = 'bbb'
-obj.hobby[0] = 'Basketball'
-console.log(obj, obj2)

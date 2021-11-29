494d1357f613ebe861b695b239342a47f9ef2b46
bycgxss <bycgxss@gmail.com>
2021-11-29 17:39:05 +0800

    forEach 空位问题

--- array/myForEach.js
+++ array/myForEach.js
@@ -1,10 +1,6 @@
 Array.prototype.myForEach = function (fn, context) {
   for (let i = 0; i < this.length; i++) {
+    if (!this.hasOwnProperty(i)) continue
     fn.call(context, this[i], i, this)
   }
 }
-
-let arr = [1, 2, 3]
-arr.myForEach(item => {
-  console.log(item)
-})
